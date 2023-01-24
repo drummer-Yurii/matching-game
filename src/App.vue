a0ba4639ee52473389091ded64cc0efcea40f50e
Yurii Rybachok <drummer2009@ukr.net>
2023-01-24 17:02:03 +0200

    add halloween styles

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <h1>Peek-a-Vue</h1>
+  <h1 class="sr-only">Peek-a-Vue</h1>
+  <img src="/images/peek-a-vue-title.png" 
+    alt="Peek-a-Vue" class="title" />
   <section class="game-board">
-    <Card 
-      v-for="(card, index) in cardList" 
-      :key="`card-${index}`" 
-      :matched="card.matched"
-      :value="card.value" 
-      :visible="card.visible"
-      :position="card.position"
-      @select-card="flipCard"
-    />
+    <Card v-for="(card, index) in cardList" :key="`card-${index}`" :matched="card.matched" :value="card.value"
+      :visible="card.visible" :position="card.position" @select-card="flipCard" />
   </section>
   <h2>{{ status }}</h2>
-  <button @click="restartGame">Restart Game</button>
+  <button @click="restartGame" class="button"><img src="/images/restart.svg" alt="Restart Icon" /> Restart Game</button>
 </template>
 
 <script>
@@ -28,7 +23,7 @@ export default {
   setup() {
     const cardList = ref([])
     const userSelection = ref([])
-    
+
     const status = computed(() => {
       if (remainingPairs.value === 0) {
         return 'Player wins!'
@@ -58,7 +53,7 @@ export default {
       })
     }
 
-    const cardItems = [1, 2, 3, 4, 5, 6, 7, 8]
+    const cardItems = ['bat', 'candy', 'cauldron', 'cupcake', 'ghost', 'moon', 'pumpkin', 'witch-hat']
 
     cardItems.forEach(item => {
       cardList.value.push({
@@ -86,7 +81,7 @@ export default {
       cardList.value[payload.position].visible = true
 
       if (userSelection.value[0]) {
-        if ((userSelection.value[0].position === payload.position) && 
+        if ((userSelection.value[0].position === payload.position) &&
           (userSelection.value[0].faceValue === payload.faceValue)
         ) {
           return
@@ -96,7 +91,7 @@ export default {
       } else {
         userSelection.value[0] = payload
       }
-    } 
+    }
 
     watch(userSelection, (currentValue) => {
       if (currentValue.length === 2) {
@@ -115,7 +110,7 @@ export default {
 
         userSelection.value.length = 0
       }
-    }, {deep: true})
+    }, { deep: true })
 
     return {
       cardList,
@@ -130,21 +125,65 @@ export default {
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  margin-top: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-image: url('../public/images/page-bg.png');
+  background-color: #00070c;
+  height: 100vh;
+  color: #fff;
+  padding-top: 60px;
+}
+
+.button {
+  background-color: orange;
+  color: white;
+  padding: 0.75rem 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  font-weight: bold;
+}
+
+.button img {
+  padding-right: 5px;
 }
 
 .game-board {
   display: grid;
-  grid-template-columns: 100px 100px 100px 100px;
-  grid-template-rows: 100px 100px 100px 100px;
-  grid-column-gap: 30px;
-  grid-row-gap: 30px;
+  grid-template-columns: repeat(4, 120px);
+  grid-template-rows: repeat(4, 120px);
+  grid-column-gap: 24px;
+  grid-row-gap: 24px;
   justify-content: center;
+}
+
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+}
+
+.title {
+  padding-bottom: 30px;
 }
 </style>
