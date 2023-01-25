37bd55044d914861d09b936444b5a9c1ddba9edc
Yurii Rybachok <drummer2009@ukr.net>
2023-01-25 15:53:48 +0200

    add confetti to win condition

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 <script>
 import _ from 'lodash'
 import { computed, ref, watch } from 'vue';
+import { launchConfetti } from './utilities/confetti';
 import Card from './components/Card.vue';
 
 export default {
@@ -91,6 +92,12 @@ export default {
         userSelection.value[0] = payload
       }
     }
+
+    watch(remainingPairs, currentValue => {
+      if (currentValue === 0) {
+        launchConfetti()
+      }
+    })
 
     watch(userSelection, (currentValue) => {
       if (currentValue.length === 2) {
